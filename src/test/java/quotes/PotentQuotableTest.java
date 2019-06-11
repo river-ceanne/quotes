34dc6d0cfcc6bb62e95d8f8b5d9c1a00f8586e4e
river-ceanne <snvencer@gmail.com>
2019-06-11 14:58:55 -0700

    added a test for appending the api quote to file

--- src/test/java/quotes/PotentQuotableTest.java
+++ src/test/java/quotes/PotentQuotableTest.java
@@ -1,14 +1,20 @@
 package quotes;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static quotes.App.getQuotesData;
 
@@ -31,6 +37,26 @@ public class PotentQuotableTest {
         App.main(strings);
 
         assertTrue(outContent.toString().contains("Author") && outContent.toString().contains("Quote"));
+
+    }
+
+    @Test
+    public void testAppendAddedQuoteToFile() throws IOException {
+
+        Quote res = PotentQuotables.requestQuote();
+
+        Path path = FileSystems.getDefault().getPath("assets", "recentquotes.json");
+        BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+        String output = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while((output = reader.readLine()) != null){
+            stringBuilder.append(output);
+        }
+
+        Gson gson = new GsonBuilder().serializeNulls().create();
+        String string = gson.toJson(res);
+        assertTrue(stringBuilder.toString().contains(string));
 
     }
 }
